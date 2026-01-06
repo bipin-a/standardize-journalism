0b46a38fa0c070e1ed30925e13e43683de93d7f2
bipin-a <aasibipin@gmail.com>
2026-01-06 01:56:37 -0500

    bug fix

---
+++ app/data/budget-category-rules.js
@@ -0,0 +1,62 @@
+export const CATEGORY_RULES = [
+  {
+    name: 'Transit',
+    plannedKeywords: ['transit commission', 'transit expansion', 'ttc', 'transit'],
+    actualKeywords: ['transit']
+  },
+  {
+    name: 'Roads & Traffic',
+    plannedKeywords: ['transportation services', 'road', 'roads', 'traffic', 'winter control', 'street lighting', 'parking'],
+    actualKeywords: ['roads', 'traffic', 'winter control', 'street lighting', 'parking']
+  },
+  {
+    name: 'Police',
+    plannedKeywords: ['police'],
+    actualKeywords: ['police']
+  },
+  {
+    name: 'Fire',
+    plannedKeywords: ['fire'],
+    actualKeywords: ['fire']
+  },
+  {
+    name: 'Paramedic',
+    plannedKeywords: ['paramedic'],
+    actualKeywords: ['paramedic']
+  },
+  {
+    name: 'Public Health',
+    plannedKeywords: ['public health'],
+    actualKeywords: ['public health', 'health']
+  },
+  {
+    name: 'Parks & Recreation',
+    plannedKeywords: ['parks', 'recreation', 'forestry'],
+    actualKeywords: ['parks', 'recreation']
+  },
+  {
+    name: 'Housing & Shelter',
+    plannedKeywords: ['housing', 'shelter'],
+    actualKeywords: ['housing', 'shelter']
+  },
+  {
+    name: 'Solid Waste',
+    plannedKeywords: ['solid waste'],
+    actualKeywords: ['solid waste', 'waste']
+  },
+  {
+    name: 'Water',
+    plannedKeywords: ['water'],
+    actualKeywords: ['water']
+  },
+  {
+    name: 'Libraries',
+    plannedKeywords: ['library'],
+    actualKeywords: ['library']
+  },
+  {
+    name: 'Corporate & Governance',
+    plannedKeywords: ['governance', 'corporate', 'city manager', 'program support', 'finance'],
+    actualKeywords: ['governance', 'corporate', 'program support']
+  }
+]
